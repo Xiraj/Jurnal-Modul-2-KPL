10680d5849638370f75aed37391e998eaaf361bc
Xiraj <akbarsirajuddinh@gmail.com>
2022-03-05 14:21:24 +0700

    menambahkan layout GUI kalkulator

--- jurnal_mod2/Form1.cs
+++ jurnal_mod2/Form1.cs
@@ -16,8 +16,6 @@ namespace jurnal_mod2
         {
             InitializeComponent();
         }
-        float number, answer;
-        int sum;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -26,10 +24,12 @@ namespace jurnal_mod2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 2;
         }
 
         private void button10_Click(object sender, EventArgs e)
@@ -38,39 +38,46 @@ namespace jurnal_mod2
 
         private void button12_Click(object sender, EventArgs e)
         {
-            compute(sum);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 3;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 4;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 5;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 6;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 7;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 8;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 9;
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
+            textBox1.Text = textBox1.Text + 0;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
